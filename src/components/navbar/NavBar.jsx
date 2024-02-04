65328c9501d7809e8c119e0c09f9f2d8fcfadc7b
Ana Lisa Veron <analisaveron36@MacBook-Air-de-Ana-3.local>
2024-02-04 15:31:01 -0300

    PreEntrega2RamosVeron

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,22 +1,27 @@
 import './NavBar.css';
 import logo from '../../assets/alv.png';
 import CartWidget from '../cartWidget/CartWidget';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 const NavBar = () => {
-    const activeLink = 'links activeLink'; //TODO add this style to the active page dinamically
+    const home = '/';
+    const tshirts = '/category/t-shirts';
+    const hoodies = '/category/hoodies';
+    const shorts = '/category/shorts';
+    const activeLink = 'links activeLink';
     const links = 'links';
-
+    const location = useLocation();
+    
     return (
         <nav id="navBar">
             <div className='column'>
                 <NavLink to="/"><img className='logo' src={logo} alt="LOGO" /></NavLink>
             </div>
             <div className='column navBarLinks'>
-                <NavLink className={activeLink} to={"/"}>Home</NavLink>
-                <NavLink className={links} to={"/category/t-shirts"}>T-shirts</NavLink>
-                <NavLink className={links} to={"/category/hoodies"}>Hoodies</NavLink>
-                <NavLink className={links} to={"/category/shorts"}>Shorts</NavLink>
+                <NavLink className={location.pathname === home? activeLink: links} to={home}>Home</NavLink>
+                <NavLink className={location.pathname === tshirts? activeLink: links} to={tshirts}>T-shirts</NavLink>
+                <NavLink className={location.pathname === hoodies? activeLink: links} to={hoodies}>Hoodies</NavLink>
+                <NavLink className={location.pathname === shorts? activeLink: links} to={shorts}>Shorts</NavLink>
             </div>
             <div className='column'>
                 <CartWidget/>
