6f4d4cdf0be3a3cec6dedac4c0ef1eb30ef381a9
Nahuel Ramos <nahuelramos8@gmail.com>
2024-02-23 16:34:14 -0300

    responsive style added to NavBar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,7 +1,9 @@
 import './NavBar.css';
 import logo from '../../assets/alv.png';
 import CartWidget from '../cartWidget/CartWidget';
+import Button from '../button/Button';
 import { NavLink, useLocation } from 'react-router-dom';
+import { useState } from 'react';
 
 const NavBar = () => {
     const home = '/';
@@ -11,9 +13,26 @@ const NavBar = () => {
     const activeLink = 'links activeLink';
     const links = 'links';
     const location = useLocation();
+    const [dropdown, setDropdown] = useState(false);
     
     return (
         <nav id="navBar">
+            <div className='navBarResponsive'>
+                <div className={dropdown? "navBarResponsiveOn": "off"}>
+                    <NavLink className={location.pathname === home? "off": links} onClick={() => setDropdown(!dropdown)} to={home}>Home</NavLink>
+                    <NavLink className={location.pathname === tshirts? "off": links} onClick={() => setDropdown(!dropdown)} to={tshirts}>T-shirts</NavLink>
+                    <NavLink className={location.pathname === hoodies? "off": links} onClick={() => setDropdown(!dropdown)} to={hoodies}>Hoodies</NavLink>
+                    <NavLink className={location.pathname === shorts? "off": links} onClick={() => setDropdown(!dropdown)} to={shorts}>Shorts</NavLink>
+                </div>
+                <Button className="navBar" value="=" onClick={() => setDropdown(!dropdown)}/>
+                <p className={activeLink}>
+                    {location.pathname == home ? "Home"
+                    : location.pathname == tshirts ? "T-shirts"
+                    : location.pathname == hoodies ? "Hoodies"
+                    : location.pathname == shorts ? "Shorts"
+                    : null}
+                </p>
+            </div>
             <div className='column'>
                 <NavLink to="/"><img className='logo' src={logo} alt="LOGO" /></NavLink>
             </div>
