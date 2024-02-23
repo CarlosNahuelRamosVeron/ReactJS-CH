ca44de10e61afa325705ae5b0fa3ea43b28a1d56
Nahuel Ramos <nahuelramos8@gmail.com>
2024-02-23 10:20:50 -0300

    Carousel style update

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,9 +1,10 @@
-import hb from '../../assets/hb.jpeg';
-import kok from '../../assets/kok.webp';
-import mySaint from '../../assets/mySaint.jpeg';
 import barnes from '../../assets/barnes.png';
 import freres from '../../assets/freres.webp';
-import notte from '../../assets/notte.png';
+import hb from '../../assets/hb.png';
+import kok from '../../assets/kok.webp';
+import mySaint from '../../assets/mySaint.jpeg';
+import notte from '../../assets/notte.webp';
+import np from '../../assets/np.png';
 import "./Carousel.css";
 
 const Carousel = () => {
@@ -16,6 +17,14 @@ const Carousel = () => {
                 <div><img src={barnes} className="imgBarnes" alt="Barnes Ind"/></div>
                 <div><img src={freres} className="imgFreres" alt="Freres"/></div>
                 <div><img src={notte} className="imgNotte" alt="Notte"/></div>
+                <div><img src={np} className="imgNP" alt="NudeProject"/></div>
+                <div><img src={mySaint} className="imgMS" alt="My Saint"/></div>
+                <div><img src={kok} className="imgKOK" alt="King of the kongo"/></div>
+                <div><img src={hb} className="imgHB" alt="Hoopers brand"/></div>
+                <div><img src={barnes} className="imgBarnes" alt="Barnes Ind"/></div>
+                <div><img src={freres} className="imgFreres" alt="Freres"/></div>
+                <div><img src={notte} className="imgNotte" alt="Notte"/></div>
+                <div><img src={np} className="imgNP" alt="NudeProject"/></div>
             </div>
             <div className='images'>
                 <div><img src={mySaint} className="imgMS" alt="My Saint"/></div>
@@ -24,6 +33,14 @@ const Carousel = () => {
                 <div><img src={barnes} className="imgBarnes" alt="Barnes Ind"/></div>
                 <div><img src={freres} className="imgFreres" alt="Freres"/></div>
                 <div><img src={notte} className="imgNotte" alt="Notte"/></div>
+                <div><img src={np} className="imgNP" alt="NudeProject"/></div>
+                <div><img src={mySaint} className="imgMS" alt="My Saint"/></div>
+                <div><img src={kok} className="imgKOK" alt="King of the kongo"/></div>
+                <div><img src={hb} className="imgHB" alt="Hoopers brand"/></div>
+                <div><img src={barnes} className="imgBarnes" alt="Barnes Ind"/></div>
+                <div><img src={freres} className="imgFreres" alt="Freres"/></div>
+                <div><img src={notte} className="imgNotte" alt="Notte"/></div>
+                <div><img src={np} className="imgNP" alt="NudeProject"/></div>
             </div>
         </div>
     )
